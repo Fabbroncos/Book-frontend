c15a6db3dbd080796d9254bb3d1f47fa1604f695
Fabbroncos <74976924+Fabbroncos@users.noreply.github.com>
2021-11-07 19:23:34 +0100

    chat

--- src/app/account/chat/chat.component.ts
+++ src/app/account/chat/chat.component.ts
@@ -1,17 +1,157 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, Input, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from "@angular/core";
+import { AuthService } from "src/app/auth/auth.service";
+import { Chat, chatMessage, ChatService } from "./chat.service";
 
 @Component({
   selector: 'app-chat-component',
   templateUrl: './chat.component.html'
 })
-export class ChatComponent implements OnInit{
+export class ChatComponent implements OnInit ,OnDestroy {
+  chats: Chat[] = []
+  activeChat: Chat;
+  messages: chatMessage[] = [];
+  page=1;
 
-  messages: String[] = []
+  connection;
+  message;
+
+  isLoading= false;
+  
+  
+  constructor(private chatService:ChatService, private authService: AuthService) {}
+
+  sendMessage( message: string){
+    this.chatService.sendMessage(this.activeChat.id,message);
+    this.message = '';
+  }
+
+  
+  scrollHeight
+  onScroll($event: Event) {
+    if ($event.target['scrollTop']<=0){
+      this.isLoading = true;
+      // const el: Element = this.messageListItem.last.nativeElement
+      this.el = this.messageListItem.last.nativeElement
+      this.chatService.getChatMessage(this.activeChat.id, this.page +1).subscribe(
+        (messageData: {data:{data: chatMessage[]}})=>{
+          this.page += 1
+          this.isLoading = false
+          this.isLoad = true
+          this.messages = this.messages.concat(this.messages ,messageData.data.data)
+          
+        }
+        
+      )
+    }
+    
+  }
+
+  @ViewChild('messagesList') messagesListEl: ElementRef
+  scrollContainer: any;
   ngOnInit() {
 
+    this.chatService.connect();
+    console.log(this.messageListItem);
+
+    
+    this.chatService.getChatList().subscribe(
+      (chatListData: {data: {data: Chat[]}})=> {
+        console.log(chatListData);
+        this.chats = chatListData.data.data;
+        this.changeChat(this.chats[0])
+        this.chatService.getMessages().subscribe( resData => {
+          console.log(resData);
+
+          
+          
+        })
+      }
+    )
   }
 
-  onInvia(message: string) {
-    this.messages.push(message);
+  isLoad: boolean = false
+  isReLoad: boolean = false
+  isFirstLoad: boolean= true;
+  el: Element;
+  lastIdex: string
+
+
+  @ViewChildren('messageListItem') messageListItem: QueryList<ElementRef>
+  getMessageSide(el: Element,message: chatMessage, isLast: boolean, isFirst: boolean) {
+    if(isFirst) {
+      el.classList.add('mb-3')
+    }
+    
+    if (isLast && this.isLoad) {
+      this.isLoad= false
+      this.el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+       })
+    }
+
+    if(isFirst && this.isFirstLoad) {
+      this.el = el
+    }
+
+    if (isLast && this.isFirstLoad) {
+      this.isFirstLoad = false
+      this.el.scrollIntoView()
+    }
+    // this.el.push(el)
+    // if(isLast && !this.isLoad) {
+    //   this.lastIdex = el.id;
+    //   this.isLoad = true
+    //   this.el[10].scrollIntoView();
+    // }
+
+    // if(isFirst && this.isReLoad) {
+    //   console.log(this.lastIdex);
+      
+    //   this.el[this.lastIdex.valueOf()].scrollIntoView({
+    //     behavior: 'auto',
+    //     block: 'center',
+    //     inline: 'center'
+    // })
+    // }
+    // if(isLast && this.isReLoad) {
+    //   this.isReLoad = false;
+    //   this.lastIdex = el.id
+    // }
+
+    if(message.sent_by === this.authService.user.value.id) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+
+  changeChat(chat: Chat) {
+    
+
+
+    this.activeChat = chat
+    this.chatService.getChatMessage(this.activeChat.id).subscribe(
+      (messageData: {data: {data: chatMessage[]}})=> {
+        console.log(messageData);
+        this.messages = messageData.data.data;
+      }
+    )
+  }
+  
+  getOtherUser(chat: Chat) {
+    if (chat.opened_user_id === this.authService.user.value.id){
+      return chat.opener_user_id
+    } else {
+      return chat.opened_user_id
+    }
+  }
+
+  ngOnDestroy() {
+    this.connection.unsubscribe();
   }
 }
+
+
+
---
+++ src/app/account/chat/chat.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { io } from "socket.io-client";
+import { AuthService } from "src/app/auth/auth.service";
+
+export interface chatMessage {
+  chat_id: number
+  created_at: Date
+  deleted_at: Date
+  id: number
+  sent_by: number
+  text: String
+  updated_at: Date
+}
+
+export interface Chat {
+  ad_id: number
+  closed_by_opened: false
+  closed_by_opener: false
+  created_at: Date
+  deleted_at: Date
+  id: number
+  last_message: chatMessage
+  last_opened_by_opened: null
+  last_opened_by_opener: null
+  opened_user_id: number
+  opener_user_id: number
+  updated_at: Date
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {  
+  private socket;
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  connect() {
+    this.socket = io("https://ws.datge.cloud/ws/libreria/chats");
+    this.socket.on('connection', (pack) => {
+      console.log(pack);
+    })
+    this.socket.emit('joining-open-chats', {jwt: this.authService.user.value.token})
+  }
+  
+  sendMessage(chat_id: number, message: string){
+    console.log(message);
+    
+    const chat = {id: chat_id, msg: message}
+    this.socket.emit('sending-message', {jwt: this.authService.user.value.token, chat: chat});    
+  }
+
+  getChatList(){
+    return this.http.get(
+      `${this.authService.url}/api/v1/chats`,
+      {
+        headers: {
+          "Authorization": this.authService.user.value.token
+        }
+      }
+    )
+  }
+
+  getChatMessage(id: number, page?: number) {
+    if (!page) {page=1}
+    return this.http.get(
+      `${this.authService.url}/api/v1/chats/${id}/messages`,
+      {
+        headers: {
+          "Authorization": this.authService.user.value.token
+        },
+        params: {
+          page: page
+        }
+      }
+    )
+  }
+  
+  getMessages() {
+    let observable = new Observable(observer => {
+      this.socket.on('receiving-message', (data) => {
+        observer.next(data);    
+      });
+      // return () => {
+      //   this.socket.disconnect();
+      // };  
+    })     
+    return observable;
+  }  
+}
